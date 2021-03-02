c1aee677fb4895e174c47577ff014e539f46f81d
ves-sev <newsecretword@yandex.ru>
2021-03-02 23:20:56 +0300

    html changed for feedback and discount, style changed for prices, style added for discount(not finished)

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,29 @@
 $(function () {
+  var granimInstance = new Granim({
+    element: '#canvas-image-blending',
+    elToSetClassOn:'#canvas-wrapper',
+    direction: 'radial',
+    isPausedWhenNotInView: true,
+    image : {
+        source: '../images/discount/discount-bgr.jpg',
+        blendingMode: 'difference',
+    },
+    states : {
+        "default-state": {
+            gradients: [
+                ['#29323c', '#485563'],
+                ['#FF6B6B', '#556270'],
+                ['#80d3fe', '#7ea0c4'],
+                ['#f0ab51', '#eceba3']
+            ],
+            transitionSpeed: 3000
+        }
+    }
+
+    
+});
+
+
   const swiper = new Swiper('.swiper-container', {
     effect: 'fade',
     fadeEffect: {
@@ -13,5 +38,4 @@ $(function () {
   });
 
 
-
-})
+});
--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,8 @@ function scripts(){
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/swiper/swiper-bundle.js',
-    'app/js/main.js'
+    'node_modules/granim/dist/granim.js',
+     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
